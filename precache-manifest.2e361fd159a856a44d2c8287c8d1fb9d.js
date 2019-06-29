58d0fe16352e08f7955b74df76bf8cff6ab7e021
Andrei Poleschuk <renardby@mail.ru>
2019-06-29 14:08:18 +0300

    feat: complite version 0.2

---
+++ precache-manifest.2e361fd159a856a44d2c8287c8d1fb9d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "cef97baad7e145fbe1f64de4495c7dcf",
+    "url": "/resume/static/media/foto.cef97baa.png"
+  },
+  {
+    "revision": "6510b67e2d20c7f02bc668d300c26b3f",
+    "url": "/resume/static/media/fontello.6510b67e.woff"
+  },
+  {
+    "revision": "18bea22a2a5d1e1d102c",
+    "url": "/resume/static/js/runtime~main.18bea22a.js"
+  },
+  {
+    "revision": "02f558b356f082fa7ff5",
+    "url": "/resume/static/js/main.02f558b3.chunk.js"
+  },
+  {
+    "revision": "d9dd611b5346ce838efa",
+    "url": "/resume/static/js/1.d9dd611b.chunk.js"
+  },
+  {
+    "revision": "02f558b356f082fa7ff5",
+    "url": "/resume/static/css/main.f64dee9b.chunk.css"
+  },
+  {
+    "revision": "d9dd611b5346ce838efa",
+    "url": "/resume/static/css/1.fb6317f7.chunk.css"
+  },
+  {
+    "revision": "c6fef90b652c92f9da7d916433f3b666",
+    "url": "/resume/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b937cc9a0343c7a99bd98c72e9abec6e.js"
+  "/resume/precache-manifest.2e361fd159a856a44d2c8287c8d1fb9d.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/resume/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
