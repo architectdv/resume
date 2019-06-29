fec2cc0b68206e9a8f473bc4c0a10b6e155ae59d
Andrei Poleschuk <renardby@mail.ru>
2019-06-29 13:53:04 +0300

    feat: complite version 0.2

---
+++ precache-manifest.b937cc9a0343c7a99bd98c72e9abec6e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "cef97baad7e145fbe1f64de4495c7dcf",
+    "url": "/static/media/foto.cef97baa.png"
+  },
+  {
+    "revision": "6510b67e2d20c7f02bc668d300c26b3f",
+    "url": "/static/media/fontello.6510b67e.woff"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "50456008f8cbd7a48635",
+    "url": "/static/js/main.50456008.chunk.js"
+  },
+  {
+    "revision": "d9dd611b5346ce838efa",
+    "url": "/static/js/1.d9dd611b.chunk.js"
+  },
+  {
+    "revision": "50456008f8cbd7a48635",
+    "url": "/static/css/main.a4c575ed.chunk.css"
+  },
+  {
+    "revision": "d9dd611b5346ce838efa",
+    "url": "/static/css/1.fb6317f7.chunk.css"
+  },
+  {
+    "revision": "35c291a34eda3e9d6cdd6e0d4c65c88a",
+    "url": "/index.html"
+  }
+];
